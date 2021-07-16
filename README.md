39dd4aa4a72b1acb70d7265dc69f392c529b4a0c
Mehul Singh Teya <mehulsaini456@gmail.com>
2021-07-16 21:57:17 +0530

    added readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Utility functions
+
+## Scripts
+1. **time_measure.cpp** -> to measure runnning time of your script
--- time_measure.cpp
+++ time_measure.cpp
@@ -4,15 +4,20 @@
 using namespace std;
 
 int main() {
+    // start time
     clock_t start_time = (float)clock();
 
+    // stress function
     for(int i = 1; i <= 30000; i++ ) {
         cout << i << " ";
     }
 
     cout << endl;
 
+    // end time
     clock_t end_time = (float)clock();
+    
+    // total run time
     cout << "Running time: " << (float)(end_time - start_time)/1000 << " seconds" << endl;
     return 0;
 }
